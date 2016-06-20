3aa2406db92f042f3b3a84b7936000ac6d5c0de4
Alejandro Cortes <Alejandro Cortes>
2016-06-20 16:57:52 -0400

    More js upodated.

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -17,14 +17,39 @@ con.connect(function(err) {
     console.log("-------------------------------------------");
     console.log('Connection established to Bamazon Database.');
     console.log("-------------------------------------------");
-    console.log("The things I have for sale..." + "\n");
+    console.log("The things I have for sale...");
 
 });
 
 con.query('SELECT * FROM Products', function(err, res) {
 	for (var i=0; i < res.length; i++) {
 
-		console.log('Department: ' + res[i].DepartmentName + '\n' + 'Product: ' + res[i].ProductName + '\n' + 'Price: $' + res[i].Price + '\n');
+		var currentStock = [];
+		currentStock.push("" + result[i].StockQuantity);
+
+		console.log('-------------------------------------------' + '\n' + 'Product: ' + res[i].ProductName + '\n' + 'Price: $' + res[i].Price 
+			+ '\n' + 'Item number: ' + res[i].ItemID + '\n' + '-------------------------------------------');
+		}
+
+		console.log("Current Stock: " + currentStock);
 	}
-	console.log("-------------------------------------------");
+
+	inquirer.prompt([{
+		type: "input",
+		name: "Item",
+		message: "Please type the Item number of the Product you would like to buy.",
+	},{
+		type: "input",
+		name: "Amount",
+		message: "How many would you like?",
+	},
+		]).then (function(choice) {
+			var itemChosen = choice.Item;
+			var amountChosen = choice.Amount;
+
+			var newStock = currentStock - amountChosen;
+			console.log('New Stock: ' + newStock);
+	});
 });
+
+
